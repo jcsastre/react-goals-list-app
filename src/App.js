6df63eb49e366ce3c710f99437cd8fbac5715eb9
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2020-04-21 12:41:21 +0200

    List of goals as state managed by React.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
 
 import GoalList from './components/GoalList/GoalList';
 import NewGoal from './components/NewGoal/NewGoal';
 
 const App = () => {
-  const courseGoals = [
+
+  const [courseGoals, setCourseGoals] = useState([
     {id: 'cg1', text: "Finish the course"},
     {id: 'cg2', text: "Learn all about the Course Main Topic"},
     {id: 'cg3', text: "Help other students in the Course Q&A"},
-  ];
+  ]);
 
   const addNewGoalHandler = (newGoal) => {
-    courseGoals.push(newGoal);
-    console.log(courseGoals);
-  }
+    // setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals((prevCourseGoals) => {
+      return prevCourseGoals.concat(newGoal);
+    });
+  };
 
   return (
     <div className="course-goals">
