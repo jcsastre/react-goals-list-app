5157a3130218c78eb4ee7dce2a083a09de8b57cd
Juan Carlos Sastre <juan.carlos.sastre.iglesias@gmail.com>
2020-04-21 12:13:02 +0200

    Components to show goals list and add new Goals.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app):
+
+```bash
+npx create-react-app quick-frontend-react
+```
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
+const App = () => {
+  const courseGoals = [
+    {id: 'cg1', text: "Finish the course"},
+    {id: 'cg2', text: "Learn all about the Course Main Topic"},
+    {id: 'cg3', text: "Help other students in the Course Q&A"},
+  ];
+
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal);
+    console.log(courseGoals);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 }
